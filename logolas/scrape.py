5e5a4e8a35bb9cf39094f163761c73b387bd0890
Matt Lewis <domattthings@gmail.com>
2019-10-27 16:04:55 -0700

    Return icon if found

--- logolas/scrape.py
+++ logolas/scrape.py
@@ -5,12 +5,18 @@ class McScraper:
     def __init__(self, site):
         self.site = site
 
+    def find_icon(self):
+        sp = self.__scrape()
+        found = sp.find('link', rel="icon")
+
+        if found:
+            return found.get('href')
+
+        return None
+
     def __scrape(self):
         r = urllib.request.urlopen(self.site)    # Makes a request to a site. Stores Response
         html = r.read() #Returns the HTML from the Response object. All HTML is in 'html'
         return BeautifulSoup(html, features="html.parser") # Parses the HTML in 'html'
-    def find_icon(self):
-        sp = self.__scrape()
 
-
-McScraper('https://nytimes.com').find_icon()
+print(McScraper('https://nytimes.com').find_icon())
