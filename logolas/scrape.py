ae3cb6a47308fbbc8b39863b86bf6959a53046b5
Andrew Menzer <andrew.menzer@gmail.com>
2019-10-27 15:46:01 -0700

    First commit

---
+++ logolas/scrape.py
@@ -0,0 +1,16 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class McScraper:
+    def __init__(self, site):
+        self.site = site
+
+    def __scrape(self):
+        r = urllib.request.urlopen(self.site)    # Makes a request to a site. Stores Response
+        html = r.read() #Returns the HTML from the Response object. All HTML is in 'html'
+        return BeautifulSoup(html, features="html.parser") # Parses the HTML in 'html'
+    def find_icon(self):
+        sp = self.__scrape()
+
+
+McScraper('https://konghq.com').find_icon()
